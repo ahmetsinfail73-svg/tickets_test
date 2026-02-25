331e6d08e8fb34998bea6c3fed7c2d560450a4c3
Fail <afail1234ahmet@gmail.com>
2026-02-25 23:14:04 +0300

    feat: configure routes and provide root taiga ui component with theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { CreateTicket } from './pages/create-ticket/create-ticket';
+import { Ticket } from './pages/ticket/ticket';
+import { Tickets } from './pages/tickets/tickets';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Tickets,
+      },
+      {
+        path: 'create',
+        component: CreateTicket,
+      },
+      {
+        path: 'tickets/:id',
+        component: Ticket,
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
-import { TuiRoot } from "@taiga-ui/core";
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TUI_DARK_MODE, TuiRoot } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('front-3');
+  protected readonly darkMode = inject(TUI_DARK_MODE);
 }
