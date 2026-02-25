3c00938a9d2fac322c1c7abfea9144232d553fbb
Fail <afail1234ahmet@gmail.com>
2026-02-25 23:00:10 +0300

    feat: add theme switcher to header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,16 +1,31 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { TuiButton, TuiPopup } from '@taiga-ui/core';
+import { WA_LOCAL_STORAGE, WA_WINDOW } from '@ng-web-apis/common';
+import { TUI_DARK_MODE, TUI_DARK_MODE_KEY, TuiButton, TuiIcon, TuiPopup } from '@taiga-ui/core';
 import { TuiDrawer } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, TuiDrawer, TuiPopup, NgTemplateOutlet, TuiButton],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    TuiDrawer,
+    TuiPopup,
+    NgTemplateOutlet,
+    TuiButton,
+    TuiIcon,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  private readonly key = inject(TUI_DARK_MODE_KEY);
+
+  private readonly storage = inject(WA_LOCAL_STORAGE);
+
+  private readonly media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
+
   protected readonly links = [
     { label: 'Заявки', path: '/' },
     { label: 'Создать заявку', path: '/create' },
@@ -20,5 +35,12 @@ export class Header {
 
   protected onClick() {
     this.open.set(!this.open());
+  }
+
+  protected readonly darkMode = inject(TUI_DARK_MODE);
+
+  protected reset() {
+    this.darkMode.set(this.media.matches);
+    this.storage?.removeItem(this.key);
   }
 }
