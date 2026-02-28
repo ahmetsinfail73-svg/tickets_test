853c1597e5ca2724d6c9465e446f3e477e5d2dd7
Fail <afail1234ahmet@gmail.com>
2026-03-01 00:58:06 +0300

    feat(ticket-form): add on submit method with validation

--- src/app/components/ticket-form/ticket-form.data.ts
+++ src/app/components/ticket-form/ticket-form.data.ts
@@ -1,39 +1,42 @@
 import { Validators } from '@angular/forms';
 import { EnumTicket, TICKET_PRIORITY, TICKET_STATUS } from '../../constants/ticket.constant';
 import { IField } from '../../models/field.model';
+import { ITicket } from '../../models/ticket.model';
 
-export const TICKET_FORM_FIELDS = (isUpdate = false): IField[] => [
+export const TICKET_FORM_FIELDS = (ticket?: ITicket): IField[] => [
   {
     key: EnumTicket.TITLE,
     label: 'Название',
     placeholder: 'Введите название...',
     icon: '@tui.folder-pen',
-    validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+    validators: [Validators.required, Validators.minLength(3), Validators.maxLength(255)],
   },
   {
     key: EnumTicket.DESCRIPTION,
     label: 'Описание',
     placeholder: 'Введите описание...',
     icon: '@tui.file-text',
-    isTextarea: true,
-    validators: [Validators.required, Validators.minLength(20), Validators.maxLength(1000)],
+    mode: 'textarea',
+    validators: [Validators.required, Validators.minLength(20)],
   },
   {
     key: EnumTicket.PRIORITY,
     label: 'Приоритет',
     icon: '@tui.octagon-alert',
     items: TICKET_PRIORITY,
+    mode: 'select',
     validators: [Validators.required],
   },
-  ...(isUpdate
-    ? [
+  ...(!!ticket
+    ? ([
         {
           key: EnumTicket.STATUS,
           label: 'Статус',
           icon: '@tui.chart-bar-big',
           items: TICKET_STATUS,
+          mode: 'select',
           validators: [Validators.required],
         },
-      ]
+      ] as IField[])
     : []),
 ];
--- src/app/components/ticket-form/ticket-form.ts
+++ src/app/components/ticket-form/ticket-form.ts
@@ -1,9 +1,15 @@
 import { Component, Input as Inp } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TuiButton } from '@taiga-ui/core';
+import { Path } from '../../constants/path.constant';
 import { IField } from '../../models/field.model';
+import { ITicket } from '../../models/ticket.model';
+import { NotificationService } from '../../services/notification.service';
+import { RouterService } from '../../services/router.service';
+import { TicketService } from '../../services/ticket.service';
 import { createFormGroup } from '../../utils/create-form.util';
 import { createInputList } from '../../utils/create-input-list.util';
+import { useLoading } from '../../utils/use-loading.util';
 import { Input } from '../inputs/input';
 import { TypeInput } from '../inputs/input-props.types';
 import { TICKET_FORM_FIELDS } from './ticket-form.data';
@@ -15,9 +21,17 @@ import { TICKET_FORM_FIELDS } from './ticket-form.data';
   styleUrl: './ticket-form.css',
 })
 export class TicketForm {
-  @Inp() defaultValues?: Record<string, any>;
+  constructor(
+    private ticketService: TicketService,
+    private notificationService: NotificationService,
+    private routerService: RouterService,
+  ) {}
+
+  @Inp() ticket?: ITicket;
 
   @Inp() isUpdate = false;
+
+  @Inp() onSuccess?: (response: any) => void;
 
   protected fields: IField[] = [];
 
@@ -25,15 +39,65 @@ export class TicketForm {
 
   protected inputs: TypeInput[] = [];
 
+  protected readonly isLoading = useLoading();
+
   ngOnInit() {
-    this.fields = TICKET_FORM_FIELDS(this.isUpdate);
+    this.fields = TICKET_FORM_FIELDS(this.ticket);
 
     this.form = createFormGroup(this.fields);
 
     this.inputs = createInputList(this.fields, this.form);
 
-    if (this.defaultValues) {
-      this.form.patchValue(this.defaultValues);
+    if (this.ticket) {
+      this.form.patchValue(this.ticket);
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.isLoading
+        .wrap(
+          this.isUpdate
+            ? this.ticketService.updateTicket({
+                ...this.form.value,
+                id: this.ticket!.id,
+              })
+            : this.ticketService.createTicket(this.form.value),
+        )
+        .subscribe({
+          next: (response) => {
+            this.notificationService.success(response.message);
+
+            if (this.isUpdate) {
+              if (this.onSuccess) {
+                this.onSuccess(response);
+              }
+            } else {
+              this.form.reset();
+
+              this.routerService.redirect({
+                path: Path.HOME,
+              });
+
+              if (this.onSuccess) {
+                this.onSuccess(response);
+              }
+            }
+          },
+          error: (error) => {
+            const errors = error?.error || {};
+
+            Object.entries(errors).forEach(([key, message]) => {
+              const control = this.form.get(key);
+
+              if (control) {
+                control.setErrors({ serverError: message });
+              }
+            });
+          },
+        });
+    } else {
+      this.notificationService.error('Пожалуйста, заполните необходимые поля');
     }
   }
 }
