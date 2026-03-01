b0fbcf973a7ae2b528eb1527b433dafaf461e3c3
Fail <afail1234ahmet@gmail.com>
2026-03-01 10:46:34 +0300

    feat(ticket): add fetch ticket

--- src/app/pages/ticket/ticket.ts
+++ src/app/pages/ticket/ticket.ts
@@ -1,15 +1,26 @@
 import { DatePipe, UpperCasePipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
-import { TuiAppearance, TuiButton, TuiDialogService, TuiExpand } from '@taiga-ui/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { TuiAppearance, TuiButton, TuiDialogService, TuiExpand, TuiLoader } from '@taiga-ui/core';
 import { TUI_CONFIRM, TuiBadge } from '@taiga-ui/kit';
 import { Heading } from '../../components/heading/heading';
+import { FileField } from '../../components/inputs/file-field/file-field';
+import { IInputProps } from '../../components/inputs/input-props.types';
 import { TicketForm } from '../../components/ticket-form/ticket-form';
+import { File } from '../../constants/file.constant';
+import { Path } from '../../constants/path.constant';
 import {
+  EnumTicket,
   TICKET_PRIORITY_APPEARANCE,
   TICKET_STATUS_APPEARANCE,
 } from '../../constants/ticket.constant';
 import { ITicket } from '../../models/ticket.model';
 import { AppearancePipe } from '../../pipes/appearance.pipe';
+import { NotificationService } from '../../services/notification.service';
+import { RouterService } from '../../services/router.service';
+import { TicketService } from '../../services/ticket.service';
+import { useLoading } from '../../utils/use-loading.util';
 
 @Component({
   selector: 'app-ticket',
@@ -23,11 +34,20 @@ import { AppearancePipe } from '../../pipes/appearance.pipe';
     TuiAppearance,
     AppearancePipe,
     TuiButton,
+    TuiLoader,
+    FileField,
   ],
   templateUrl: './ticket.html',
   styleUrl: './ticket.css',
 })
-export class Ticket {
+export class Ticket implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private ticketService: TicketService,
+    private routerService: RouterService,
+    private notificationService: NotificationService,
+  ) {}
+
   protected readonly expanded = signal(false);
 
   protected readonly priorityAppearance = TICKET_PRIORITY_APPEARANCE;
@@ -36,19 +56,58 @@ export class Ticket {
 
   private readonly dialog = inject(TuiDialogService);
 
-  protected ticket = {
-    id: 1,
-    title: 'Не работает VPN на удаленном рабочем месте',
-    description:
-      'Подробности инцидента: пользователи фиксируют нестабильную работу. Нужен анализ и отчет.',
-    priority: 'high',
-    status: 'in_progress',
-    createdAt: '2026-02-22T17:31:15.676Z',
-  } as ITicket;
+  protected readonly isLoading = useLoading();
+
+  protected readonly ticket = signal<ITicket | null>(null);
+
+  protected readonly fileControl = new FormControl();
+
+  protected fileProps!: IInputProps;
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+
+    this.fetchTicket(id);
+  }
+
+  protected onSuccess() {
+    const ticket = this.ticket();
+
+    if (ticket) {
+      this.fetchTicket(ticket.id.toString());
+    }
+  }
+
+  private fetchTicket(id: string) {
+    this.isLoading.wrap(this.ticketService.getTicket(id)).subscribe({
+      next: (ticket) => {
+        this.ticket.set(ticket);
+
+        this.fileProps = {
+          key: EnumTicket.ATTACHMENTS,
+          label: 'Файлы',
+          control: this.fileControl,
+          icon: '@tui.paperclip',
+          mode: 'file',
+          defaultValue: ticket.attachments,
+          meta: {
+            ticketId: ticket.id,
+            maxCount: File.MAX_COUNT,
+            accept: File.ACCEPT,
+          },
+        };
+      },
+      error: (error) => {
+        this.routerService.redirect({
+          path: Path.HOME,
+        });
+      },
+    });
+  }
 
   protected onDelete() {
     this.dialog
-      .open(TUI_CONFIRM, {
+      .open<boolean>(TUI_CONFIRM, {
         size: 'm',
         label: 'Вы уверены, что хотите удалить эту заявку?',
         data: {
@@ -57,6 +116,18 @@ export class Ticket {
           no: 'Отмена',
         },
       })
-      .subscribe(console.log);
+      .subscribe((result) => {
+        if (result) {
+          this.ticketService.deleteTicket(this.ticket()!.id).subscribe({
+            next: (response) => {
+              this.notificationService.success(response.message);
+
+              this.routerService.redirect({
+                path: Path.HOME,
+              });
+            },
+          });
+        }
+      });
   }
 }
